93a79a398db2caf362725d9126b33e6fb5b3ed56
tankTopTaro <balmoresk3v1n@gmail.com>
2024-09-05 15:30:37 +0800

    moved the generations array to a separate file

---
+++ src/objects/Generations.js
@@ -0,0 +1,14 @@
+const Generations = [
+    { id: 0, name: 'All Generations', value: 'all-generation' },
+    { id: 1, name: 'Generation I', value: 'generation-i' },
+    { id: 2, name: 'Generation II', value: 'generation-ii' },
+    { id: 3, name: 'Generation III', value: 'generation-iii' },
+    { id: 4, name: 'Generation IV', value: 'generation-iv' },
+    { id: 5, name: 'Generation V', value: 'generation-v' },
+    { id: 6, name: 'Generation VI', value: 'generation-vi' },
+    { id: 7, name: 'Generation VII', value: 'generation-vii' },
+    { id: 8, name: 'Generation VIII', value: 'generation-viii' },
+    { id: 9, name: 'Generation IX', value: 'generation-ix' },
+]
+
+export default Generations
--- src/views/Pokedex.vue
+++ src/views/Pokedex.vue
@@ -2,6 +2,7 @@
 import { computed, ref } from 'vue';
 import Pokemon from './Pokemon.vue';
 import Modal from './components/Modal.vue';
+import Generations from '../objects/Generations'
 
 const props = defineProps({
     pokedex: Object
@@ -12,18 +13,6 @@ const emit = defineEmits(['palette'])
 // Modal
 const selectedGeneration = ref({ id: 0, name: 'All Generations', value: 'all-generation' })
 const isModalOpen = ref(false)
-const generations = [
-    { id: 0, name: 'All Generations', value: 'all-generation' },
-    { id: 1, name: 'Generation I', value: 'generation-i' },
-    { id: 2, name: 'Generation II', value: 'generation-ii' },
-    { id: 3, name: 'Generation III', value: 'generation-iii' },
-    { id: 4, name: 'Generation IV', value: 'generation-iv' },
-    { id: 5, name: 'Generation V', value: 'generation-v' },
-    { id: 6, name: 'Generation VI', value: 'generation-vi' },
-    { id: 7, name: 'Generation VII', value: 'generation-vii' },
-    { id: 8, name: 'Generation VIII', value: 'generation-viii' },
-    { id: 9, name: 'Generation IX', value: 'generation-ix' },
-]
 
 const openModal = () => {
     isModalOpen.value = true
@@ -123,5 +112,5 @@ const prevBox = () => {
         </div>
     </div>
     <!-- Modal -->
-    <Modal :isOpen="isModalOpen" :generations="generations" @close="closeModal" @select="handleGenerationSelect" />
+    <Modal :isOpen="isModalOpen" :generations="Generations" @close="closeModal" @select="handleGenerationSelect" />
 </template>
